16ab67ead55c0ea9f37d783d2fd3369df2f07775
Tucker Fritz <tuckerpfritz@gmail.com>
2024-12-03 13:41:19 -0600

    Year 2024, Day 3, Part 2

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -5,17 +5,21 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func solution1() {
+func getInput() string {
 	buf, err := os.ReadFile("./input.txt")
 	if err != nil {
 		panic(err)
 	}
+	return string(buf)
+}
 
+func evaluateMulExpressions(expressions string) int {
 	regex := regexp.MustCompile("mul\\((\\d{1,3}),(\\d{1,3})\\)")
 
-	matches := regex.FindAllStringSubmatch(string(buf), -1)
+	matches := regex.FindAllStringSubmatch(expressions, -1)
 
 	runningTotal := 0
 	for i := 0; i < len(matches); i++ {
@@ -31,9 +35,31 @@ func solution1() {
 		runningTotal += num1 * num2
 	}
 
-	fmt.Printf("Part 1 Solution: %d\n", runningTotal)
+	return runningTotal
+}
+
+func solution1() {
+	input := getInput()
+
+	total := evaluateMulExpressions(input)
+
+	fmt.Printf("Part 1 Solution: %d\n", total)
+}
+
+func solution2() {
+	runningTotal := 0
+	input, before, after := getInput(), "", ""
+	found := true
+	for found {
+		before, after, _ = strings.Cut(input, "don't()")
+		runningTotal += evaluateMulExpressions(before)
+		_, input, found = strings.Cut(after, "do()")
+	}
+
+	fmt.Printf("Part 2 Solution: %d\n", runningTotal)
 }
 
 func main() {
 	solution1()
+	solution2()
 }
