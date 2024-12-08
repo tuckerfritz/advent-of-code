e240e911d983c3459b4a0b70fbad5e9030fb9af6
Tucker Fritz <tuckerpfritz@gmail.com>
2024-12-07 20:57:26 -0600

    Year 2024, Day 6, Part 2

--- 2024/day6/main.go
+++ 2024/day6/main.go
@@ -46,12 +46,31 @@ func getInitialCoords(grid [][]string) [2]int {
 	panic("Could not find starting position!")
 }
 
-func processRoute(grid [][]string, directions map[string][2]int, x int, y int, numPositions int) int {
+func isLoop(visited map[[2]int][]string, guard string, x int, y int) bool {
+	coords := [2]int{x, y}
+	guardPermutations, exists := visited[coords]
+	if !exists {
+		visited[coords] = []string{guard}
+		return false
+	}
+	for i := 0; i < len(guardPermutations); i++ {
+		if guard == guardPermutations[i] {
+			return true
+		}
+	}
+	visited[coords] = append(visited[coords], guard)
+	return false
+}
+
+func processRoute(grid [][]string, visited map[[2]int][]string, directions map[string][2]int, x int, y int, numPositions int) (map[[2]int][]string, int, bool) {
 	guard := grid[x][y]
 	direction := directions[guard]
 	nextX, nextY := direction[0]+x, direction[1]+y
+	if isLoop(visited, guard, x, y) {
+		return visited, numPositions, true
+	}
 	if nextX > len(grid)-1 || nextX < 0 || nextY > len(grid[x])-1 || nextY < 0 {
-		return numPositions + 1
+		return visited, numPositions + 1, false
 	}
 	if grid[nextX][nextY] == "#" {
 		if guard == ">" {
@@ -63,15 +82,15 @@ func processRoute(grid [][]string, directions map[string][2]int, x int, y int, n
 		} else if guard == "^" {
 			grid[x][y] = ">"
 		}
-		return processRoute(grid, directions, x, y, numPositions)
+		return processRoute(grid, visited, directions, x, y, numPositions)
 	} else if grid[nextX][nextY] == "X" {
 		grid[x][y] = "X"
 		grid[nextX][nextY] = guard
-		return processRoute(grid, directions, nextX, nextY, numPositions)
+		return processRoute(grid, visited, directions, nextX, nextY, numPositions)
 	} else if grid[nextX][nextY] == "." {
 		grid[x][y] = "X"
 		grid[nextX][nextY] = guard
-		return processRoute(grid, directions, nextX, nextY, numPositions+1)
+		return processRoute(grid, visited, directions, nextX, nextY, numPositions+1)
 	}
 	panic("Unknown character in input!")
 }
@@ -82,15 +101,46 @@ func printGrid(grid [][]string) {
 	}
 }
 
-func solution1() {
+func solution1() map[[2]int][]string {
 	grid := getInput()
 	directions := getDirections()
 	coords := getInitialCoords(grid)
-	numPositions := processRoute(grid, directions, coords[0], coords[1], 0)
-	printGrid(grid)
+	visited, numPositions, _ := processRoute(grid, make(map[[2]int][]string), directions, coords[0], coords[1], 0)
 	fmt.Printf("Part 1 Solution: %d\n", numPositions)
+	return visited
+}
+
+func copyGrid(grid [][]string) [][]string {
+	duplicate := make([][]string, len(grid))
+	for i := range grid {
+		duplicate[i] = make([]string, len(grid[i]))
+		copy(duplicate[i], grid[i])
+	}
+	return duplicate
+}
+
+func solution2(visited map[[2]int][]string) {
+	originalGrid := getInput()
+	directions := getDirections()
+	coords := getInitialCoords(originalGrid)
+	totalObstaclePositions := 0
+
+	for visitedCoords := range visited {
+		grid := copyGrid(originalGrid)
+		if coords[0] == visitedCoords[0] && coords[1] == visitedCoords[1] {
+			continue
+		}
+		grid[visitedCoords[0]][visitedCoords[1]] = "#"
+		_, _, isLoop := processRoute(grid, make(map[[2]int][]string), directions, coords[0], coords[1], 0)
+		if isLoop {
+			totalObstaclePositions += 1
+		}
+		grid[visitedCoords[0]][visitedCoords[1]] = "."
+	}
+	fmt.Printf("Part 2 Solution: %d\n", totalObstaclePositions)
 }
 
 func main() {
-	solution1()
+	visited := solution1()
+	solution2(visited)
 }
